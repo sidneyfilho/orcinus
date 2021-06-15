96d6e763500131e3790ee63607f7cfdc03db35bf
SidneyCFilho <sidpfilho@gmail.com>
2021-06-14 23:03:39 -0300

    Initial commit

---
+++ src/main/java/com/vixtecnologia/orcinus/OrcinusApplication.java
@@ -0,0 +1,13 @@
+package com.vixtecnologia.orcinus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcinusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcinusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vixtecnologia/orcinus/OrcinusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vixtecnologia.orcinus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcinusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
